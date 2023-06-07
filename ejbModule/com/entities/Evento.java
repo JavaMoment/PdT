43453009984105fb222e00e26c8ccb9623262769
Romina Caubarrere <rominacaubarrere@gmail.com>
2023-06-07 18:58:51 -0300

    itr, reclamo y evento

---
+++ ejbModule/com/entities/Evento.java
@@ -0,0 +1,212 @@
+package com.entities;
+
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+
+/**
+ * The persistent class for the EVENTOS database table.
+ * 
+ */
+@Entity
+@Table(name="EVENTOS")
+@NamedQuery(name="Evento.findAll", query="SELECT e FROM Evento e")
+public class Evento implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@SequenceGenerator(name="EVENTOS_IDEVENTO_GENERATOR", sequenceName="EVENTOS_SEQ_ID")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="EVENTOS_IDEVENTO_GENERATOR")
+	@Column(name="ID_EVENTO")
+	private long idEvento;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="FECHA_HORA_FINAL")
+	private Date fechaHoraFinal;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="FECHA_HORA_INICIO")
+	private Date fechaHoraInicio;
+
+	private String titulo;
+
+	//bi-directional many-to-many association to Analista
+	@ManyToMany
+	@JoinTable(
+		name="ANALISTA_EVENTO"
+		, joinColumns={
+			@JoinColumn(name="ID_EVENTO")
+			}
+		, inverseJoinColumns={
+			@JoinColumn(name="ID_ANALISTA")
+			}
+		)
+	private List<Analista> analistas;
+/*
+	//bi-directional many-to-one association to Constancia
+	@OneToMany(mappedBy="evento")
+	private List<Constancia> constancias;
+
+	//bi-directional many-to-one association to EstudianteEvento
+	@OneToMany(mappedBy="evento")
+	private List<EstudianteEvento> estudianteEventos;
+
+	//bi-directional many-to-one association to Justificacione
+	@OneToMany(mappedBy="evento")
+	private List<Justificaciones> justificaciones;
+
+	//bi-directional many-to-one association to Reclamo
+	@OneToMany(mappedBy="evento")
+	private List<Reclamo> reclamos;
+
+
+	//bi-directional many-to-many association to Tutore
+	@ManyToMany(mappedBy="eventos")
+	private List<Tutor> tutores;
+*/
+	public Evento() {
+	}
+
+	public long getIdEvento() {
+		return this.idEvento;
+	}
+
+	public void setIdEvento(long idEvento) {
+		this.idEvento = idEvento;
+	}
+
+	public Date getFechaHoraFinal() {
+		return this.fechaHoraFinal;
+	}
+
+	public void setFechaHoraFinal(Date fechaHoraFinal) {
+		this.fechaHoraFinal = fechaHoraFinal;
+	}
+
+	public Date getFechaHoraInicio() {
+		return this.fechaHoraInicio;
+	}
+
+	public void setFechaHoraInicio(Date fechaHoraInicio) {
+		this.fechaHoraInicio = fechaHoraInicio;
+	}
+
+	public String getTitulo() {
+		return this.titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public List<Analista> getAnalistas() {
+		return this.analistas;
+	}
+
+	public void setAnalistas(List<Analista> analistas) {
+		this.analistas = analistas;
+	}
+
+	/*
+	public List<Constancia> getConstancias() {
+		return this.constancias;
+	}
+
+	public void setConstancias(List<Constancia> constancias) {
+		this.constancias = constancias;
+	}
+
+	public Constancia addConstancia(Constancia constancia) {
+		getConstancias().add(constancia);
+		constancia.setEvento(this);
+
+		return constancia;
+	}
+
+	public Constancia removeConstancia(Constancia constancia) {
+		getConstancias().remove(constancia);
+		constancia.setEvento(null);
+
+		return constancia;
+	}
+
+	public List<EstudianteEvento> getEstudianteEventos() {
+		return this.estudianteEventos;
+	}
+
+	public void setEstudianteEventos(List<EstudianteEvento> estudianteEventos) {
+		this.estudianteEventos = estudianteEventos;
+	}
+
+	public EstudianteEvento addEstudianteEvento(EstudianteEvento estudianteEvento) {
+		getEstudianteEventos().add(estudianteEvento);
+		estudianteEvento.setEvento(this);
+
+		return estudianteEvento;
+	}
+
+	public EstudianteEvento removeEstudianteEvento(EstudianteEvento estudianteEvento) {
+		getEstudianteEventos().remove(estudianteEvento);
+		estudianteEvento.setEvento(null);
+
+		return estudianteEvento;
+	}
+
+	public List<Justificaciones> getJustificaciones() {
+		return this.justificaciones;
+	}
+
+	public void setJustificaciones(List<Justificaciones> justificaciones) {
+		this.justificaciones = justificaciones;
+	}
+
+	public Justificacione addJustificacione(Justificacione justificacione) {
+		getJustificaciones().add(justificacione);
+		justificacione.setEvento(this);
+
+		return justificacione;
+	}
+
+	public Justificacione removeJustificacione(Justificacione justificacione) {
+		getJustificaciones().remove(justificacione);
+		justificacione.setEvento(null);
+
+		return justificacione;
+	}
+
+	public List<Reclamo> getReclamos() {
+		return this.reclamos;
+	}
+
+	public void setReclamos(List<Reclamo> reclamos) {
+		this.reclamos = reclamos;
+	}
+
+	public Reclamo addReclamo(Reclamo reclamo) {
+		getReclamos().add(reclamo);
+		reclamo.setEvento(this);
+
+		return reclamo;
+	}
+
+	public Reclamo removeReclamo(Reclamo reclamo) {
+		getReclamos().remove(reclamo);
+		reclamo.setEvento(null);
+
+		return reclamo;
+	}
+	
+
+	public List<Tutor> getTutores() {
+		return this.tutores;
+	}
+
+	public void setTutores(List<Tutor> tutores) {
+		this.tutores = tutores;
+	}
+
+*/
+}
---
+++ ejbModule/com/entities/Itr.java
@@ -0,0 +1,102 @@
+package com.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+//import entities.Departamento;
+//import entities.Usuario;
+
+import java.util.List;
+
+
+/**
+ * The persistent class for the ITR database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Itr.findAll", query="SELECT i FROM Itr i")
+public class Itr implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@SequenceGenerator(name="ITR_IDITR_GENERATOR", sequenceName="ITR_SEQ_ID")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="ITR_IDITR_GENERATOR")
+	@Column(name="ID_ITR")
+	private long idItr;
+
+	private String nombre;
+
+	/*
+	//bi-directional many-to-one association to Departamento
+	@OneToMany(mappedBy="itr")
+	private List<Departamento> departamentos;
+
+	//bi-directional many-to-one association to Usuario
+	@OneToMany(mappedBy="itr")
+	private List<Usuario> usuarios;
+*/
+	public Itr() {
+	}
+
+	public long getIdItr() {
+		return this.idItr;
+	}
+
+	public void setIdItr(long idItr) {
+		this.idItr = idItr;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/*
+	
+	public List<Departamento> getDepartamentos() {
+		return this.departamentos;
+	}
+
+	public void setDepartamentos(List<Departamento> departamentos) {
+		this.departamentos = departamentos;
+	}
+
+	public Departamento addDepartamento(Departamento departamento) {
+		getDepartamentos().add(departamento);
+		departamento.setItr(this);
+
+		return departamento;
+	}
+
+	public Departamento removeDepartamento(Departamento departamento) {
+		getDepartamentos().remove(departamento);
+		departamento.setItr(null);
+
+		return departamento;
+	}
+
+	public List<Usuario> getUsuarios() {
+		return this.usuarios;
+	}
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
+	public Usuario addUsuario(Usuario usuario) {
+		getUsuarios().add(usuario);
+		usuario.setItr(this);
+
+		return usuario;
+	}
+
+	public Usuario removeUsuario(Usuario usuario) {
+		getUsuarios().remove(usuario);
+		usuario.setItr(null);
+
+		return usuario;
+	}
+*/
+}
