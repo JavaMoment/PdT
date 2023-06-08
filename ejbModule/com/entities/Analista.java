9e83259211e801ab25d158000126834317f333d8
redex22 <n3xtfix@gmail.com>
2023-06-08 16:43:46 -0300

    Refactor: Cambio el nombre de la secuencia para que coincida con BDD y no genere conflictos

--- ejbModule/com/entities/Analista.java
+++ ejbModule/com/entities/Analista.java
@@ -14,7 +14,7 @@ public class Analista implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@SequenceGenerator(name="ANALISTAS_IDANALISTA_GENERATOR", sequenceName="ANALISTAS_ID_SEQ", allocationSize=1, initialValue=1)
+	@SequenceGenerator(name="ANALISTAS_IDANALISTA_GENERATOR", sequenceName="analistas_id_seq", allocationSize=1, initialValue=1)
 	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="ANALISTAS_IDANALISTA_GENERATOR")
 	@Column(name="ID_ANALISTA", unique=true, nullable=false, precision=38)
 	private long idAnalista;
